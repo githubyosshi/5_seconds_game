0f648497b1eb9d289f5c9f398c876e60b4cc7be2
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-02-26 18:48:38 +0900

    Style_change_by_result_commit

--- js/main.js
+++ js/main.js
@@ -12,9 +12,15 @@
 
   stop.addEventListener('click', function() {
     var elapsedTime;  // 経過時間の変数宣言
+    var diff;
     elapsedTime = (Date.now() - startTime) / 1000;
     // elapsedTime = 4;
     // result.textContent = elapsedTime;     // resultに表示
     result.textContent = elapsedTime.toFixed(3);   // 小数点3位まで停止時間表示
+    diff = elapsedTime - 5.0;             // 5秒に近い値
+    // if (diff > -1.0 && < 1.0) {           // 1秒前後
+    if (Math.abs(diff) < 0.5) {   // 絶対値（マイナスも正数に置換え）で比較
+      result.className = 'perfect';
+    }
   });
 })();
